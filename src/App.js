9d2f1d39f43cbddef145777e9c4f7dd186180e36
logiblue <karanikolasms@gmail.com>
2022-12-07 00:21:50 +0200

    cleanup beta - deploy netlify

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import './App.css';
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Alert from 'react-bootstrap/Alert';
-import { Button } from './button';
-import { ListComponent } from './ListComponent';
 import Gist from "react-gist";
 
 
